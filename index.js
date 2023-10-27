d166618330eadc14668b4a9bb1fbb3d079657c7b
Rasul Aslanov <68597254+xin0ps@users.noreply.github.com>
2023-10-27 23:26:37 +0400

    newStory

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+const dark = document.getElementById("dark");
 
+window.onmousemove = function(e) {
+    const x = e.x / window.innerWidth * 100;
+    const y = e.y / window.innerHeight * 100; 
+    dark.style.background = `radial-gradient(circle at ${x}% ${y}%, transparent, rgba(0,0,0,.9) 75px, black)`;
+}
