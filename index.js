b48df059b12bb76ccf5ad410bc9288f73cb9a5b8
Rasul Aslanov <68597254+xin0ps@users.noreply.github.com>
2023-10-28 15:52:02 +0400

    Update index.js

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const dark = document.getElementById("dark");
 window.onmousemove = function(e) {
     const x = e.x / window.innerWidth * 100;
     const y = e.y / window.innerHeight * 100; 
-    dark.style.background = `radial-gradient(circle at ${x}% ${y}%, transparent, rgba(0,0,0,.9) 75px, black)`;
+    dark.style.background = `radial-gradient(circle at ${x}% ${y}%, transparent, rgba(0,0,0,.9) 100px, black)`;
 }
